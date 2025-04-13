ef76a9469e8b1f0e63206d6be85ebb50e0dc3431
johannes <johannes.lewen@lewen.eu>
2025-04-13 12:16:01 +0200

    provide a method to display the snowman in its current state

--- src/snowman.py
+++ src/snowman.py
@@ -2,21 +2,24 @@
 
 class Snowman:
     def __init__(self):
-        self.melted = False
+        self.is_melted = False
         self.melted_levels = 0
         self.ascii_graphic = r"""         ___  
         /___\ 
         (o o)
         ( : )
         ( : )"""
-        print(self.ascii_graphic)
+        self.show_snowman()
         self.new_ascii_lines = [idx for idx,ltr in enumerate(self.ascii_graphic) if ltr == '\n']
+
+    def show_snowman(self):
+        print(self.ascii_graphic)
 
     def remove_level(self):
         self.melted_levels += 1
         self.ascii_graphic = self.ascii_graphic[:self.new_ascii_lines.pop()]
         if self.melted_levels >= 3:
-            self.melted = True
+            self.is_melted = True
             print(f'The snowman has completely melted :\'( ')
         print(self.ascii_graphic)
 
