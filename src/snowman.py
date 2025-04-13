db0648c3829a794f2cc4ca7c3314274627c71bb5
johannes <johannes.lewen@lewen.eu>
2025-04-13 10:21:48 +0200

    v1 of the snowman actually melting

--- src/snowman.py
+++ src/snowman.py
@@ -1,22 +1,31 @@
-
+### Handle the snowman graphic and it's melting progress
 
 class Snowman:
     def __init__(self):
-        self.counter = 0
-        self.ascii_graphic = """
-    
-         ___  
+        self.melted = False
+        self.melted_levels = 0
+        self.ascii_graphic = r"""         ___  
         /___\ 
-        (o o) 
-        ( : ) 
-        ( : ) 
-        
-     """
+        (o o)
+        ( : )
+        ( : )"""
+        print(self.ascii_graphic)
+        self.new_ascii_lines = [idx for idx,ltr in enumerate(self.ascii_graphic) if ltr == '\n']
 
-    def _remove_level(self):
-        pass
+    def remove_level(self):
+        if self.melted_levels < 3:
+            self.ascii_graphic = self.ascii_graphic[:self.new_ascii_lines.pop()]
+            self.melted_levels += 1
+        else:
+            self.melted = True
+            print(f'The snowman has completely melted :\'( ')
+        print(self.ascii_graphic)
 
 
 if __name__ == '__main__':
     snowman = Snowman()
-    
+    i = 0
+    while i < 5:
+        snowman.remove_level()
+        i += 1
+
