b13832b6c80394590ac45efc9911502a753961be
johannes <johannes.lewen@lewen.eu>
2025-04-13 10:50:36 +0200

    finish v1 of the guessing game

--- src/game.py
+++ src/game.py
@@ -1,8 +1,11 @@
 import os
+from copy import copy
 from random import randint
 
-WORD_CATALOGUE = ['Alemannia', 'github', 'world','Deepthought']
+from snowman import Snowman
 
+WORD_CATALOGUE = ['alemannia', 'github', 'world','deepthought']
+CORRECT_GUESSES = []
 
 class Word:
     def __init__(self):
@@ -20,17 +23,42 @@ def __demo(word):
         print(f'I chose the word "{word.word}".')
         print(f'It contains {len(word.word)} "{word.word_underscores}".\n')
 
+def _check_solution_status(word):
+    word_guessed = False
+    solution = copy(word.word)
+    for correct_guess in CORRECT_GUESSES:
+        solution = solution.replace(correct_guess, '')
+    if not len(solution) >= 1:
+        word_guessed = True
+    return word_guessed
 
-def start_game():
-    guess = input('Please guess a letter: ').lower()
-    print(guess)
+
+def add_guess_to_correct_guesses(word, guess) -> bool:
+    CORRECT_GUESSES.append(guess)
+    word_guessed = _check_solution_status(word)
+    return word_guessed
+
+
+def start_game(word, snowman):
+    while not snowman.melted:
+        guess = input('Please guess a letter: ').lower()
+        if guess in word.word:
+            print(f'YESSS! "{guess}" is in the word we are looking for!')
+            word_completely_guessed = add_guess_to_correct_guesses(word, guess)
+            if word_completely_guessed:
+                print(f'CONGRATULATIONS!! You guessed all the letters of "{word.word}" correctly!')
+                break
+        else:
+            print(f'Unfortunately "{guess}" is not correct.')
+            snowman.remove_level()
 
 
 def main() -> None:
     print('Hello from your Snowman game!')
     word = Word()
+    snowman = Snowman()
     __demo(word)
-    start_game()
+    start_game(word, snowman)
 
 
 if __name__ == '__main__':
